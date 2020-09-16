df3ee598f39d2666014b36bc44005df43e024140
Rahul Dwiwedi <rahul.dwiwedi@decisionpoint.in>
2020-09-17 01:31:16 +0530

    login html page created

--- algoscale/login/urls.py
+++ algoscale/login/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 
 urlpatterns = [
-
+    path(r'login', user_login),
 ]
--- algoscale/login/views.py
+++ algoscale/login/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.template.context_processors import csrf
+
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/profile')
+    context = {}
+    context.update(csrf(request))
+    return render(request, 'login.html', context)
+
