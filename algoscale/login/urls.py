ec45e35c4ee5899a873452526d310bada1b431e4
Rahul Dwiwedi <rahul.dwiwedi@decisionpoint.in>
2020-09-17 01:49:57 +0530

    login flow done

--- algoscale/login/urls.py
+++ algoscale/login/urls.py
@@ -5,4 +5,6 @@ from .views import *
 
 urlpatterns = [
     path(r'login', user_login),
+    path(r'auth', auth_view),
+
 ]
--- algoscale/login/views.py
+++ algoscale/login/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.template.context_processors import csrf
+from django.contrib import auth
 
 
 def user_login(request):
@@ -13,3 +14,16 @@ def user_login(request):
     context.update(csrf(request))
     return render(request, 'login.html', context)
 
+
+def auth_view(request):
+    username = request.POST.get('username', "")
+    password = request.POST.get('psw', "")
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        print(request.user.username)
+        return HttpResponseRedirect('/profile')
+    else:
+        msg = "Wrong Username or Password ....Please Try again."
+        return render(request, 'login.html', {'msg': msg})
